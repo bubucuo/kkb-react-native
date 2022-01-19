0155eb03329ff68f8b0f588ef9f7cd47499d32ce
bubucuo <276185876@qq.com>
2022-01-19 16:35:36 +0800

    save

--- lesson4/src/routers/App.js
+++ lesson4/src/routers/App.js
@@ -7,6 +7,15 @@ import {Provider} from 'react-redux';
 import store from '@/store/index';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 
+import {AMapSdk} from 'react-native-amap3d';
+
+AMapSdk.setApiKey(
+  Platform.select({
+    android: 'c52c7169e6df23490e3114330098aaac',
+    ios: '2e301afe760e3c98d9f0bd894d5ea4c5',
+  }),
+);
+
 export default function App() {
   return (
     <SafeAreaProvider>
